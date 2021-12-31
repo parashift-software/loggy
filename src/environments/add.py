da2467351d2eeead0534a77e61871ed96a1d91b1
Grayson Kuhns <grayson@parashift.xyz>
2021-12-31 14:22:07 -0600

    SNAPSHOT

--- src/environments/add.py
+++ src/environments/add.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from cliff.command import Command
@@ -15,12 +16,24 @@ class EnvironmentsAdd(Command):
 
     def get_parser(self, prog_name):
         parser = super(EnvironmentsAdd, self).get_parser(prog_name)
-        parser.add_argument('name')
+        parser.add_argument('environment_name')
+        parser.add_argument('destination_arn')
+        parser.add_argument('iam_role_arn')
         return parser
 
     def take_action(self, parsed_args):
-        environment_name = parsed_args.name
-        environments_svc = EnvironmentsService(self.app.config['dynamodb'])
+        # Load required inputs
+        environment_name = parsed_args.environment_name
+        destination_arn = parsed_args.destination_arn
+        iam_role_arn = parsed_args.iam_role_arn
 
-        self.log.info(f'Creating environment \'{environment_name}\'')
-        environments_svc.create(parsed_args.name)
+        # Init services
+        environments_svc = EnvironmentsService(self.app.config['dynamodb'])
+        ingestion_streams_svc = LogIngestionStreamsService(self.app.config['dynamodb'])
+
+        # Create environment and ingestion stream
+        environment = environments_svc.create(parsed_args.name)
+        self.log.info(f'Created environment: {json.dumps(environment)}')
+
+        stream = ingestion_streams_svc.create(destination_arn, iam_role_arn, environment['id'])
+        self.log.info(f'Created log ingestion stream: {json.dumps(stream)}')
