e82d343fb6a5d9b3ab050f01e0146880b0b6f433
Grayson Kuhns <grayson@parashift.xyz>
2022-01-01 13:41:40 -0600

    LOGGY-26 Show log group tags during audit process

--- src/services/log_groups_service.py
+++ src/services/log_groups_service.py
@@ -13,20 +13,27 @@ class CloudWatchLogGroupService:
         iterator = paginator.paginate()
         for page in iterator:
             for log_group in page.get('logGroups', []):
-                log_group['subscriptionFilters'] = self.get_subscription_filters(log_group.get('logGroupName'))
+                log_group_name = log_group['logGroupName']
+                log_group['subscriptionFilters'] = self.get_subscription_filters(log_group_name)
+                log_group['tags'] = self.get_tags(log_group_name)
                 log_groups.append(log_group)
 
         return log_groups
 
-    def get_subscription_filters(self, log_group):
+    def get_subscription_filters(self, log_group_name):
         subscription_filters = []
 
         paginator = self._client.get_paginator('describe_subscription_filters')
-        iterator = paginator.paginate(logGroupName=log_group)
+        iterator = paginator.paginate(logGroupName=log_group_name)
         for page in iterator:
             subscription_filters.extend(page.get('subscriptionFilters', []))
 
         return subscription_filters
+
+    def get_tags(self, log_group_name):
+        return self._client.list_tags_log_group(
+            logGroupName=log_group_name
+        ).get('tags', {})
 
     def subscribe_to_kinesis(self, log_group_name, destination_arn, iam_role_arn):
         self._client.put_subscription_filter(
